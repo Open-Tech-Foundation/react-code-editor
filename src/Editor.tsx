f6a78d4fce8b0ff8a3fb93e1f2be983ccc57907a
Thanga Ganapathy <ganapathy888@gmail.com>
2023-04-29 09:48:41 +0530

    Add line numbers to the editor

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,70 +1,96 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import highlightSyntax from "./highlightSyntax";
+import React, { useState } from 'react';
+import Header from './Header';
+import highlightSyntax from './highlightSyntax';
 
 export default function Editor({
   code,
   themes = [],
-  theme = "Dark",
+  theme = 'Dark',
   style,
-  title = "",
+  title = '',
 }) {
   const [value, setValue] = useState(code);
   const curTheme = themes.find((i) => i.name === theme);
-  const hlCode = highlightSyntax(value, "js", curTheme);
+  const hlCode = highlightSyntax(value, 'js', curTheme);
+
+  const newLinesMatch = [...hlCode.matchAll(/\n/g)];
 
   return (
     <div
       style={{
         ...style,
-        border: "1px solid lightgray",
-        boxSizing: "border-box",
+        border: '1px solid lightgray',
+        boxSizing: 'border-box',
       }}
     >
       <Header title={title} />
       <div
         style={{
-          boxSizing: "border-box",
+          boxSizing: 'border-box',
           color: curTheme.color,
           background: curTheme.background,
-          height: "calc(100% - 30px)",
-          overflow: "hidden auto",
-          padding: "10px 0px",
+          height: 'calc(100% - 30px)',
+          overflow: 'hidden auto',
+          display: 'grid',
+          gridTemplateColumns: 'auto 1fr',
         }}
       >
         <div
           style={{
-            position: "relative",
-            textAlign: "left",
-            padding: "0px",
-            overflow: "hidden",
+            backgroundColor: '#f5f5f5',
+            color: '#6c6c6c',
+            borderRight: '1px solid #ddd',
+            padding: '10px 0px',
+          }}
+        >
+          {new Array(newLinesMatch.length + 1).fill('').map((a, i) => (
+            <div
+              key={i}
+              style={{
+                textAlign: 'right',
+                marginBottom: '2px',
+                fontSize: '14px',
+                padding: '0px 5px',
+              }}
+            >
+              {i + 1}
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            boxSizing: 'border-box',
+            position: 'relative',
+            textAlign: 'left',
+            padding: '0px',
+            overflow: 'hidden',
           }}
         >
           <pre
             style={{
-              userSelect: "none",
-              margin: "0px",
-              border: "0px",
-              background: "none",
-              boxSizing: "inherit",
-              display: "inherit",
-              fontFamily: "inherit",
-              fontSize: "inherit",
-              fontStyle: "inherit",
-              fontVariantLigatures: "inherit",
-              fontWeight: "inherit",
-              letterSpacing: "inherit",
-              lineHeight: "inherit",
-              tabSize: "inherit",
-              textIndent: "inherit",
-              textRendering: "inherit",
-              textTransform: "inherit",
-              whiteSpace: "pre-wrap",
-              wordBreak: "keep-all",
-              overflowWrap: "break-word",
-              position: "relative",
-              pointerEvents: "none",
-              padding: "10px",
+              userSelect: 'none',
+              margin: '0px',
+              border: '0px',
+              background: 'none',
+              boxSizing: 'inherit',
+              display: 'inherit',
+              fontFamily: 'inherit',
+              fontSize: 'inherit',
+              fontStyle: 'inherit',
+              fontVariantLigatures: 'inherit',
+              fontWeight: 'inherit',
+              letterSpacing: 'inherit',
+              lineHeight: 'inherit',
+              tabSize: 'inherit',
+              textIndent: 'inherit',
+              textRendering: 'inherit',
+              textTransform: 'inherit',
+              whiteSpace: 'pre-wrap',
+              wordBreak: 'keep-all',
+              overflowWrap: 'break-word',
+              position: 'relative',
+              pointerEvents: 'none',
+              padding: '10px',
             }}
             dangerouslySetInnerHTML={{ __html: hlCode }}
           />
@@ -73,37 +99,37 @@ export default function Editor({
             value={value}
             onChange={(e) => setValue(e.target.value)}
             style={{
-              margin: "0px",
-              border: "0px",
-              background: "none",
-              boxSizing: "inherit",
-              display: "inherit",
-              fontFamily: "inherit",
-              fontSize: "inherit",
-              fontStyle: "inherit",
-              fontVariantLigatures: "inherit",
-              fontWeight: "inherit",
-              letterSpacing: "inherit",
-              lineHeight: "inherit",
-              tabSize: "inherit",
-              textIndent: "inherit",
-              textRendering: "inherit",
-              textTransform: "inherit",
-              whiteSpace: "pre-wrap",
-              wordBreak: "keep-all",
-              overflowWrap: "break-word",
-              position: "absolute",
-              top: "0px",
-              left: "0px",
-              height: "100%",
-              width: "100%",
-              resize: "none",
-              color: "inherit",
-              overflow: "hidden",
-              WebkitFontSmoothing: "antialiased",
-              WebkitTextFillColor: "transparent",
-              padding: "10px",
-              outline: "none",
+              margin: '0px',
+              border: '0px',
+              background: 'none',
+              boxSizing: 'inherit',
+              display: 'inherit',
+              fontFamily: 'inherit',
+              fontSize: 'inherit',
+              fontStyle: 'inherit',
+              fontVariantLigatures: 'inherit',
+              fontWeight: 'inherit',
+              letterSpacing: 'inherit',
+              lineHeight: 'inherit',
+              tabSize: 'inherit',
+              textIndent: 'inherit',
+              textRendering: 'inherit',
+              textTransform: 'inherit',
+              whiteSpace: 'pre-wrap',
+              wordBreak: 'keep-all',
+              overflowWrap: 'break-word',
+              position: 'absolute',
+              top: '0px',
+              left: '0px',
+              height: '100%',
+              width: '100%',
+              resize: 'none',
+              color: 'inherit',
+              overflow: 'hidden',
+              WebkitFontSmoothing: 'antialiased',
+              WebkitTextFillColor: 'transparent',
+              padding: '10px',
+              outline: 'none',
             }}
           />
         </div>
