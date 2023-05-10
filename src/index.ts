9a0ba23efb42ae2a32913df392b2d3af26a7c681
Thanga Ganapathy <ganapathy888@gmail.com>
2023-05-11 01:48:40 +0530

    Remove github themes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import Editor from './Editor';
 
-// Themes
-import githubTheme from './themes/githubTheme';
-import githubDarkTheme from './themes/githubDarkTheme';
-
-export { Editor, githubTheme, githubDarkTheme };
+export { Editor };
